2c6c116f70b2c409c32e8767f9fbc31a55524638
Jacob MacNeil <1macneiljac@hdsb.ca>
2022-01-29 11:42:43 -0500

    Added climb code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,19 +7,28 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel; 
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
  
-  // Make a reference to the Motor Controller
-  public static TalonFX motor = new TalonFX(10);
-                                      //    ^ set this number to the ID in Phoenix tuner
-
-  // Make a reference to the Xbox Controller
+  public static TalonSRX motor1 = new TalonSRX(55);
+  public static TalonFX motor2 = new TalonFX(46);
+  public CANSparkMax rightClimber = new CANSparkMax(49, CANSparkMaxLowLevel.MotorType.kBrushless);
+  public CANSparkMax leftClimber = new CANSparkMax(48, CANSparkMaxLowLevel.MotorType.kBrushless);
+  Solenoid climberBrake = new Solenoid(10,PneumaticsModuleType.CTREPCM,4);
+  Compressor compressor = new Compressor(10, PneumaticsModuleType.CTREPCM);
+  public static TalonSRX motor = new TalonSRX(41);
   public static XboxController controller = new XboxController(0);
-                                                         //    ^ set this number to the index in driverstation
   @Override
   public void robotInit() {}
   
@@ -38,14 +47,22 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    double forward = controller.getRightTriggerAxis();
-    double reverse = controller.getLeftTriggerAxis();
-    //comment
-    double output = forward - reverse;
-    output = output;
-
-    // Set the output of the motor controller to 100% in the forward direction
-    motor.set(ControlMode.PercentOutput, output);
+    //double forward = controller.getRightTriggerAxis();
+  //double reverse = controller.getLeftTriggerAxis();
+  //double output = forward - reverse;
+ // motor2.set(ControlMode.PercentOutput, output);
+ compressor.enableDigital();
+ var output = controller.getLeftY();
+  if(Math.abs(output) > 0.10) {
+    rightClimber.set(-(output*0.90));
+    leftClimber.set(-(output*0.90));
+    climberBrake.set(true);
+  } 
+  else{
+    rightClimber.set(-0.02);
+    leftClimber.set(-0.02);
+    climberBrake.set(false);
+  }
   }
 
   @Override
