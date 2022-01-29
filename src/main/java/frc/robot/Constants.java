c293844f1c4bcd9cea92b0660220dd7fa5beefe6
Marc Levesque <qormix@gmail.com>
2022-01-29 15:11:10 -0500

    Added example drivetrain

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static int kDRIVE_LEFT_A_NODE_ID = 0;
+    public static int kDRIVE_LEFT_B_NODE_ID = 1;
+
+    public static int kDRIVE_RIGHT_A_NODE_ID = 3;
+    public static int kDRIVE_RIGHT_B_NODE_ID = 4;
+}
---
+++ src/main/java/frc/robot/subsystems/RobotDrive.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.*;
+
+public class RobotDrive extends SubsystemBase {
+ 
+  public final TalonSRX left = new TalonSRX(kDRIVE_LEFT_A_NODE_ID);
+  public final TalonSRX right = new TalonSRX(kDRIVE_LEFT_A_NODE_ID);
+
+  public RobotDrive() {
+    new VictorSPX(kDRIVE_LEFT_B_NODE_ID).follow(left);
+    new VictorSPX(kDRIVE_RIGHT_B_NODE_ID).follow(right);
+  }
+
+  public void setArcadeDrive(double forward, double rotation) {
+    left.set(ControlMode.PercentOutput, forward + rotation);
+    right.set(ControlMode.PercentOutput, forward - rotation);
+  }
+}
