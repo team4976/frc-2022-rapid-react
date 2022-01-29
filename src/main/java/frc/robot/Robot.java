7fcbfeaaed913f8c36ac0c2a8db135259271e93e
Colten Gladwish <coltengladwishipad@icloud.com>
2022-01-29 11:42:38 -0500

    Partial Climber code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,61 +4,138 @@
 
 package frc.robot;
 
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
- 
-  // Make a reference to the Motor Controller
-  public static TalonFX motor = new TalonFX(10);
-                                      //    ^ set this number to the ID in Phoenix tuner
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  // Make a reference to the Xbox Controller
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+
+  public static TalonFX falcon = new TalonFX(46);
+  public static TalonSRX talon = new TalonSRX(49);
+  public static CANSparkMax rightClimber; 
+  private CANSparkMax leftClimber; 
   public static XboxController controller = new XboxController(0);
-                                                         //    ^ set this number to the index in driverstation
+
   @Override
   public void robotInit() {
-    //comment added and changed
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+
+    rightClimber = new CANSparkMax(49, CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftClimber = new CANSparkMax(48, CANSparkMaxLowLevel.MotorType.kBrushless);
 
   }
-  
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
   public void robotPeriodic() {}
 
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
 
+  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
 
+  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
-  
+
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
 
-    double forward = controller.getRightTriggerAxis();
-    double reverse = controller.getLeftTriggerAxis();
+    //Deadband is 0.1
 
-    double output = forward - reverse;
+    double reverse = controller.getRightTriggerAxis();
+    double forward = controller.getLeftTriggerAxis();
 
-    // Set the output of the motor controller to 100% in the forward direction
-    motor.set(ControlMode.PercentOutput, output);
+    double joystick = controller.getLeftY();
+
+    double Output = (forward - reverse);
+
+    falcon.set(ControlMode.PercentOutput, Output);
+  
+  if (Math.abs(joystick) > 0.1 ){
+    leftClimber.set(ControlMode.PercentOutput, joystick);
+    rightClimber.set(ControlMode.PercentOutput, joystick);
+  }
+  else{
+    leftClimber.set(ControlMode.PercentOutput, 0);
+    rightClimber.set(ControlMode.PercentOutput, 0);
   }
 
+  }
+
+  /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {}
 
+  /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {}
 
+  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {}
 
+  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
