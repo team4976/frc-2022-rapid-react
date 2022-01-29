a72e108547343840b45601232669c4bc8624b6be
Fuzzydudes <KierstenMarieS14@gmail.com>
2022-01-29 11:40:16 -0500

    Added shooter code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,12 +14,17 @@ import edu.wpi.first.wpilibj.XboxController;
 public class Robot extends TimedRobot {
  
   // Make a reference to the Motor Controller
-  public static TalonFX motor = new TalonFX(10);
+  public static TalonFX motor = new TalonFX(1);
                                       //    ^ set this number to the ID in Phoenix tuner
 
   // Make a reference to the Xbox Controller
   public static XboxController controller = new XboxController(0);
                                                          //    ^ set this number to the index in driverstation
+  int ID;
+  public static TalonFX motor2 = new TalonFX(45);
+  public static TalonFX motor3 = new TalonFX(10);
+  double percent = .4;
+
   @Override
   public void robotInit() {}
   
@@ -44,7 +49,22 @@ public class Robot extends TimedRobot {
     double output = forward - reverse;
 
     // Set the output of the motor controller to 100% in the forward direction
-    motor.set(ControlMode.PercentOutput, output);
+    motor3.set(ControlMode.PercentOutput, output);
+    if(controller.getAButton()){
+    motor.set(ControlMode.PercentOutput, -percent);
+    motor2.set(ControlMode.PercentOutput, percent);
+    int pov = controller.getPOV();
+    if(pov == 0){
+      percent = percent + 0.01;
+      if(percent > 1){percent = 1;}
+    }else if(pov == 180){
+      percent = percent - 0.01;
+      if(percent < -1){percent = -1;}
+    }
+    }else{
+      motor.set(ControlMode.PercentOutput, 0);
+      motor2.set(ControlMode.PercentOutput, 0);
+    }
   }
 
   @Override
