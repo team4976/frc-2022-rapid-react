61beb8ea24e3071d21c769b2d24f0b989d5916d8
MrPassmore <cpassmore@flodraulic.com>
2022-02-03 20:44:39 -0500

    In Prog

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.*;
+
+public class Shooter extends SubsystemBase {
+
+    TalonFX shooter = new TalonFX(kShooterMotorANodeID);
+
+    public void ShooterSpeed(double speed) {
+        shooter.set(ControlMode.Velocity, speed);
+      }
+
+}
