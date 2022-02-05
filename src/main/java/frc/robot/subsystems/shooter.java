9210ccab8c3ddfefa315f15ceb99b32f38a146ac
RevoltRobotics <36797984+RevoltRobotics@users.noreply.github.com>
2022-02-05 09:24:49 -0500

    create Indexer

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -2,18 +2,24 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.*;
 
 public class Shooter extends SubsystemBase {
 
+    DigitalInput indexSensor = new DigitalInput(1);
+
     TalonFX shooter = new TalonFX(kSHOOTER_A_NODE_ID);
 
-    public void ShooterSpeed(double speed) {
+    public void seShooterSpeed(double speed) {
         shooter.set(ControlMode.Velocity, speed);
-      }
+    }
+
+    public boolean ballAtIndexer(){
+        return indexSensor.get()
+    }
 
 }
