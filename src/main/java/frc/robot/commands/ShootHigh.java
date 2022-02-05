c2999c1d6cf869495b185bd063bd26ef30b29575
RevoltRobotics <36797984+RevoltRobotics@users.noreply.github.com>
2022-02-05 09:01:45 -0500

    Finished shooterHigh, renamed the Shooter subsystem

--- src/main/java/frc/robot/commands/ShootHigh.java
+++ src/main/java/frc/robot/commands/ShootHigh.java
@@ -1,17 +1,21 @@
 package frc.robot.commands;
 
+import frc.robot.subsystems.Shooter;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import frc.robot.subsystems.shooter;
 
 public class ShootHigh extends CommandBase {
-    shooter shooter;
+    
+    Shooter shooter;
 
-
-    public shootHigh(shooter shooter){
+    public ShootHigh (Shooter shooter){
         this.shooter = shooter;
-        set
+        addRequirements(shooter);
     }
 
-
+    @Override
+    public void initialize(){
+        super.initialize();
+    }
 }
--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -2,15 +2,18 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.*;
 
+public class Shooter extends SubsystemBase {
 
-public class shooter extends SubsystemBase {
-    TalonFX shooterMotor = new TalonFX(kSHOOTER_A_NODE_ID);
-    public void setShooterSpeed(double speed) {
-        shooterMotor.set(ControlMode.Velocity, speed);
-    }
+    TalonFX shooter = new TalonFX(kSHOOTER_A_NODE_ID);
+
+    public void ShooterSpeed(double speed) {
+        shooter.set(ControlMode.Velocity, speed);
+      }
+
 }
