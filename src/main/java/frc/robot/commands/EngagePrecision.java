4fb6ce7dc650ed94a1f1fd865411f799f330ed65
Jacob MacNeil <1macneiljac@hdsb.ca>
2022-02-12 11:57:07 -0500

    Created turbo/precision modes

---
+++ src/main/java/frc/robot/commands/EngagePrecision.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RobotDrive;
+public class EngagePrecision extends CommandBase {
+    private RobotDrive robotDrive;
+
+    public EngagePrecision(RobotDrive robotDrive){
+        this.robotDrive = robotDrive;
+        addRequirements(robotDrive);
+    }
+
+    public boolean isFinished(){
+        return false;
+    }
+
+    public void initialize(){
+        robotDrive.setPrecisionMode();
+    }
+
+    public void end(boolean interupted){
+        robotDrive.endPrecisionMode();
+    }
+}
