2320cafb978cd9679ffc0de9b67479df094a1e15
Colten Gladwish <coltengladwishipad@icloud.com>
2022-02-26 15:09:41 -0500

    created 2 commands for auto

--- src/main/java/frc/robot/commands/RunAuto.java
+++ src/main/java/frc/robot/commands/RunAuto.java
@@ -2,14 +2,24 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 public class RunAuto extends SequentialCommandGroup {
     
     public RunAuto() {
+        Intake intake;
+        Shooter shoot;
+        Elevator elv;
+        FlywheelShooter fly;
         addCommands(
             new ParallelCommandGroup(
                 // Start the Shooter
+                new SpoolHigh(fly),
+
                 // Start the intake
+                new IntakeBall(intake)
             )  
         );
     }
