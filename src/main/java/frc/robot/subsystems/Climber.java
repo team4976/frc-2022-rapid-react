7f42add281ae98891039c1259207549ca6688aa0
Anthony Mitchell <1mitchellant@hdsb.ca>
2022-02-26 14:53:38 -0500

    Climber Encoder

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
@@ -18,7 +20,7 @@ private RelativeEncoder m_encoder;
 
 public Solenoid solenoidclimber = new Solenoid(kCLIMBER_MODLE_A_NODE_ID, PneumaticsModuleType.CTREPCM, kCLIMBER_CHANNEL_A_NODE_ID);
 
-public CANSparkMax rightClimber = new CANSparkMax(kCLIMBER_LEFT_A_NODE_ID, MotorType.kBrushed);//CANSparkMaxLowLevel.MotorType.kBrushless);
+public CANSparkMax rightClimber = new CANSparkMax(kCLIMBER_LEFT_A_NODE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);//CANSparkMaxLowLevel.MotorType.kBrushless);
 public CANSparkMax leftClimber = new CANSparkMax(kCLIMBER_RIGHT_A_NODE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
 
@@ -30,7 +32,7 @@ public void zeroencoder(){
 
 public void extendarm(double ext, double ret){
 
-double Height = ((RelativeEncoder) rightClimber).getPosition();
+double Height = m_encoder.getPosition();
 
 solenoidclimber.set(true);
 if ((Height >= 100 && ext-ret > 0) || (Height <= 0 && ext-ret < 0)){
