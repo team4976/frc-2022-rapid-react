b6410b522e9155ba53762eee734bf454b13fdd80
Fuzzydudes <KierstenMarieS14@gmail.com>
2022-02-26 11:06:35 -0500

    Its broken

--- src/main/java/frc/robot/subsystems/AutoAim.java
+++ src/main/java/frc/robot/subsystems/AutoAim.java
@@ -1,37 +1,43 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+
+
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.*;
+
 
 public class AutoAim extends SubsystemBase{
     
     public static PIDController controller = new PIDController(0.1, 0.01, 0);
     public static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
-    public static void target(){    
+    private static RobotDrive _drive;
+
+
+    public static boolean hasValidTarget(){
+        double tv = table.getEntry("tv").getDouble(0);
+        return tv > 0;
+    }
+
+    public static void target(){   
         double tx  = table.getEntry("tx").getDouble(0);
         double ty = table.getEntry("ty").getDouble(0);
         System.out.println(tx + ", " + ty);
         if(hasValidTarget()){
         double horizontalAdjust = controller.calculate(tx,0);
         double verticalAdjust = controller.calculate(ty, 0);
-        RobotDrive.left.set(ControlMode.PercentOutput, horizontalAdjust + verticalAdjust);//It should autocalculate them both, it might freak the hell out
-        RobotDrive.right.set(ControlMode.PercentOutput, -horizontalAdjust + verticalAdjust);
-
-
+        _drive.setArcadeDrive(verticalAdjust, horizontalAdjust);
+        System.out.println(horizontalAdjust);
     }else{
-        RobotDrive.left.set(ControlMode.PercentOutput, 0);
-        RobotDrive.right.set(ControlMode.PercentOutput, 0);
+        _drive.setArcadeDrive(0, 0);
+
         System.out.println("Theres no target");
 
     }
 
     }
-    public static boolean hasValidTarget(){
-        double tv = table.getEntry("tv").getDouble(0);
-        System.out.println(tv);
-        return tv > 0;
-    }
+    
 }
+//AHHHHHHHHHH
