f319b800f849a61737584d2d396c260d9855fb41
Christian Verghis <chris.verghis@gmail.com>
2022-03-22 16:50:19 -0400

    Added missing

--- src/main/java/frc/robot/commands/Auto/MoveToPos.java
+++ src/main/java/frc/robot/commands/Auto/MoveToPos.java
@@ -6,8 +6,8 @@ import frc.robot.subsystems.RobotDrive;
 public class MoveToPos extends SequentialCommandGroup{
     public MoveToPos(RobotDrive robotDrive){
         addCommands(
-            new AutoDrivePos(robotDrive, 12000),
-            new AutoDrivePos(robotDrive, -12000)
+            new AutoDrive(robotDrive, 0.5, 10000, 0)
+            //new AutoDrivePos(robotDrive, -12000)
         );
     }
     
